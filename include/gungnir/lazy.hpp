37cfd70a1a212d7a9c53c390aa4f1e2b2ba78318
Zizheng Tai <zizheng.tai@gmail.com>
2016-05-03 09:25:31 -0700

    refined LazyVal implementation

---
+++ include/gungnir/lazy.hpp
@@ -0,0 +1,158 @@
+/*
+ * Copyright 2016 Zizheng Tai
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file gungnir/lazy.hpp
+ * Utilities for lazy evaluation.
+ */
+
+#ifndef GUNGNIR_LAZY_HPP
+#define GUNGNIR_LAZY_HPP
+
+#include <memory>
+#include <functional>
+#include <utility>
+
+//#include "gungnir/detail/lazy.hpp"
+#include "gungnir/detail/util.hpp"
+
+namespace gungnir {
+
+using namespace detail;
+
+/**
+ * A lazily constructed value.
+ *
+ * @author Zizheng Tai
+ * @since 1.0
+ * @tparam T the type of the underlying value
+ * @tparam Args the types of the arguments passed to the constructor of `T`
+ */
+template<typename T, typename... Args>
+class LazyVal {
+public:
+    /**
+     * Constructs a lazy value from the given arguments.
+     *
+     * @param args the arguments passed to the constructor of `T`
+     */
+    explicit LazyVal(Args&&... args) noexcept
+        : args_(std::forward<Args>(args)...)
+    {}
+
+    /** Deleted copy constructor. */
+    LazyVal(const LazyVal &) = delete;
+
+    /** Default move constructor. */
+    LazyVal(LazyVal &&) = default;
+
+    /** Deleted copy assignment operator. */
+    LazyVal & operator=(const LazyVal &) = delete;
+
+    /** Default move assignment operator. */
+    LazyVal & operator=(LazyVal &&) = default;
+
+    /**
+     * Returns the underlying value, constructing it if necessary.
+     *
+     * @return the underlying value
+     */
+    const T & get() const
+    {
+        if (val_ == nullptr) {
+            create(typename GenSeq<sizeof... (Args)>::type());
+        }
+        return *val_;
+    }
+
+    /**
+     * Returns the underlying value, constructing it if necessary.
+     *
+     * @return the underlying value
+     */
+    T & get()
+    {
+        return const_cast<T &>(
+                static_cast<const LazyVal<T, Args...> *>(this)->get());
+    }
+
+    /**
+     * Returns the underlying value, constructing it if necessary.
+     *
+     * @return the underlying value
+     */
+    operator const T &() const
+    {
+        return get();
+    }
+
+    /**
+     * Returns the underlying value, constructing it if necessary.
+     *
+     * @return the underlying value
+     */
+    operator T &()
+    {
+        return get();
+    }
+
+    /**
+     * Returns the underlying value, constructing it if necessary.
+     *
+     * @return the underlying value
+     */
+    const T & operator()() const
+    {
+        return get();
+    }
+
+    /**
+     * Returns the underlying value, constructing it if necessary.
+     *
+     * @return the underlying value
+     */
+    T & operator()()
+    {
+        return get();
+    }
+
+private:
+    template<std::size_t... S>
+    void create(Seq<S...>) const
+    {
+        val_.reset(new T(std::move(std::get<S>(args_))...));
+    }
+
+    mutable std::tuple<Decay<Args>...> args_;
+    mutable std::unique_ptr<T> val_;
+};
+
+/**
+ * Returns a value that will be lazily constructed from the given arguments.
+ *
+ * @tparam T the type of the underlying value
+ * @tparam Args the types of the arguments passed to the constructor of `T`
+ * @param args the arguments passed to the constructor of `T`
+*/
+template<typename T, typename... Args>
+LazyVal<T, Args...> lazyVal(Args&&... args)
+{
+    return LazyVal<T, Args...>(std::forward<Args>(args)...);
+}
+
+}  // namespace gungnir
+
+#endif  // GUNGNIR_LAZY_HPP
---
+++ test/lazy/test_lazy_val.cpp
@@ -0,0 +1,153 @@
+#include <memory>
+
+#include "catch.hpp"
+
+#include "gungnir/lazy.hpp"
+using gungnir::lazyVal;
+
+TEST_CASE("test lazyVal", "[lazyVal]") {
+
+    using UP = std::unique_ptr<int>;
+    using SP = std::shared_ptr<int>;
+
+    static std::size_t count = 0;
+
+    class Foo {
+    public:
+        Foo() noexcept { ++count; }
+
+        Foo(UP up) noexcept : up(std::move(up)) { ++count; }
+
+        Foo(SP sp) noexcept : sp(std::move(sp)) { ++count; }
+
+        Foo(UP up, SP sp) noexcept : up(std::move(up)), sp(std::move(sp)) { ++count; }
+
+        Foo(Foo &&) = default;
+
+        UP up;
+        SP sp;
+    };
+
+    SECTION("constructor with no arguments") {
+        const auto p = lazyVal<UP>();
+        REQUIRE(p() == nullptr);
+
+        count = 0;
+
+        const auto foo = lazyVal<Foo>();
+        REQUIRE(count == 0);
+
+        foo();
+        REQUIRE(count == 1);
+
+        foo();
+        REQUIRE(count == 1);
+    }
+    SECTION("constructor with non-copyable arguments") {
+        const auto p = lazyVal<UP>(new int(123));
+        REQUIRE(*p() == 123);
+
+        count = 0;
+
+        const auto foo = lazyVal<Foo>(UP(new int(123)));
+        REQUIRE(count == 0);
+
+        REQUIRE(*foo().up == 123);
+        REQUIRE(count == 1);
+
+        foo();
+        REQUIRE(count == 1);
+    }
+    SECTION("constructor with copyable arguments") {
+        count = 0;
+
+        const auto foo = lazyVal<Foo>(SP(new int(123)));
+        REQUIRE(count == 0);
+
+        REQUIRE(*foo().sp == 123);
+        REQUIRE(count == 1);
+
+        REQUIRE(foo().sp.use_count() == 1);
+        REQUIRE(count == 1);
+
+        const auto p = std::make_shared<int>(456);
+        const auto bar = lazyVal<Foo>(p);
+        REQUIRE(p.use_count() == 2);
+        REQUIRE(count == 1);
+
+        REQUIRE(*bar().sp == 456);
+        REQUIRE(count == 2);
+
+        REQUIRE(bar().sp.use_count() == 2);
+        REQUIRE(count == 2);
+
+        const auto poi = lazyVal<Foo>(std::cref(p));
+        REQUIRE(p.use_count() == 2);
+        REQUIRE(count == 2);
+
+        REQUIRE(*poi().sp == 456);
+        REQUIRE(count == 3);
+
+        REQUIRE(poi().sp.use_count() == 3);
+        REQUIRE(count == 3);
+    }
+    SECTION("constructor with both copyable and non-copyable arguments") {
+        count = 0;
+
+        const auto foo = lazyVal<Foo>(UP(new int(123)), SP(new int(456)));
+        REQUIRE(count == 0);
+
+        REQUIRE(*foo().up == 123);
+        REQUIRE(count == 1);
+
+        REQUIRE(*foo().sp == 456);
+        REQUIRE(count == 1);
+
+        REQUIRE(foo().sp.use_count() == 1);
+        REQUIRE(count == 1);
+
+        UP up(new int(654));
+        SP sp(new int(321));
+        const auto bar = lazyVal<Foo>(std::move(up), sp);
+        REQUIRE(sp.use_count() == 2);
+        REQUIRE(count == 1);
+
+        REQUIRE(*bar().up == 654);
+        REQUIRE(count == 2);
+
+        REQUIRE(*bar().sp == 321);
+        REQUIRE(count == 2);
+
+        REQUIRE(bar().sp.use_count() == 2);
+        REQUIRE(count == 2);
+    }
+    SECTION("mutable values") {
+        count = 0;
+
+        auto foo = lazyVal<Foo>(UP(new int(123)), SP(new int(456)));
+        REQUIRE(count == 0);
+
+        auto bar = std::move(foo);
+        REQUIRE(count == 0);
+
+        REQUIRE(*bar().up == 123);
+        REQUIRE(count == 1);
+
+        REQUIRE(*bar().sp == 456);
+        REQUIRE(bar().sp.use_count() == 1);
+        REQUIRE(count == 1);
+
+        auto poi = std::move(bar());
+        REQUIRE(count == 1);
+
+        REQUIRE(bar().up == nullptr);
+        REQUIRE(count == 1);
+
+        REQUIRE(bar().sp == nullptr);
+        REQUIRE(count == 1);
+
+        REQUIRE(*poi.up == 123);
+        REQUIRE(*poi.sp == 456);
+        REQUIRE(poi.sp.use_count() == 1);
+    }
+}
